bf943f0a684c79172182c6a1df28a2ba5d03e1fd
MahdiHDev <mahdi.hussain.work@gmail.com>
2025-03-24 03:16:15 +0600

    setup vercel config

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.json({ message: 'Welcome to chat app' });
